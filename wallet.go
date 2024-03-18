b79b53b947665da678824de96588575ca0c34988
Haneyah Seemein(INTERNSHIP) <haniya23seemein@gmail.com>
2024-03-18 17:16:35 +0530

    Requirement 2.0

--- wallet.go
+++ wallet.go
@@ -1,5 +1,41 @@
 package Wallet
 
-func RupeeToDollar(rupee float) float{
-	return float(rupee)*82.47
+type Currency struct{
+	valueInINR float
+}
+
+// rupeecoin := Currency{valueInINR: 1}
+// dollarcoin := Currency{valueInINR: 82.47}
+
+func createCurrency(value float) Currrency{
+	return Currency{valueInINR: value}
+}
+
+func initialiseCurrencies(){
+	const rupeecoin := createCurrency(1)
+	const dollarcoin := createCurrency(82.47)
+}
+
+type Wallet struct{
+	var balanceInINR float
+}
+
+func newWallet(amount float){
+	return Wallet{balanceInINR: amount}
+}
+
+func ConvertCurrency(from, to Currency) float {
+	return from.valueInINR/to.valueInINR
+}
+
+func Credit(c Currency, amount float){
+
+}
+
+func RupeeToDollar(amount float){
+	return amount*ConvertCurrency(rupeecoin, dollarcoin)
+}
+
+func DollarToRupee(amount float){
+	return amount*ConvertCurrency(rupeecoin, dollarcoin)
 }
--- wallet_test.go
+++ wallet_test.go
@@ -1,15 +1,12 @@
 package Wallet
 
-
 import "testing"
 
 func TestRupeeToDollar(t *testing.T){
-	rupee := 1
+	got := RupeeToDollar(82.47)
+	want := 1
 
-	want := 82.47
-	got := RupeeToDollar(rupee)
-
-	if(want!=got){
-		t.Errorf("wrong conversion")
+	if got!=want{
+		t.Errorf("Wrong conversion")
 	}
 }
